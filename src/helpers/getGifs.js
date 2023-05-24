d33b871f78c6b267bc8d5e990df372be90bd3146
Jessica Villa <suhkha92@gmail.com>
2023-05-24 17:16:29 -0600

    testing getGifs helper

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,13 +1,15 @@
-export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=8q7fTYQbHxiiGbkINpmv4yhvq6PNsauu&q=${ category }&limit=10`;
-    const response = await fetch(url);
-    const {data} = await response.json();
+import "whatwg-fetch";
 
-    const gifs = data.map(img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }))
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=8q7fTYQbHxiiGbkINpmv4yhvq6PNsauu&q=${category}&limit=10`;
+  const response = await fetch(url);
+  const { data } = await response.json();
 
-    return gifs;
-}
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  return gifs;
+};
